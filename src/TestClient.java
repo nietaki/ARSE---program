c5dbf0af4e1b659e26f2deaf0f7d433cd1c9dc97
Jacek K <nietaki@gmail.com>
2011-05-23 14:07:33 +0200

    request i odpowiedz

--- src/TestClient.java
+++ src/TestClient.java
@@ -6,18 +6,33 @@ public class TestClient {
 	public static void main(String args[]) {
 		try {
 			Socket skt = new Socket("localhost", 1234);
+			
+			DataOutputStream disout = new DataOutputStream(skt.getOutputStream());
+			String appId = StaticHelper.getAppId();
+			String classnameHash = StaticHelper.getClassnameHash(Boolean.class);
+			int len = 3;
+			String msg = "lol";
+			disout.writeBytes(appId);
+			disout.writeBytes(classnameHash);
+			disout.writeInt(len);
+			//TODO fix if we're sending some kind of multibyte bullshit
+			disout.writeChars(msg);
+			disout.flush();
 			BufferedReader in = new BufferedReader(new
 					InputStreamReader(skt.getInputStream()));
-			System.out.print("Received string: '");
+			
 
 			while (!in.ready()) {}
-			System.out.println(in.readLine()); // Read one line and output it
+			System.out.print("Received string: '");
+			char[] inmsg = new char[2];
+			in.read(inmsg,0, 2); // Read one line and output it
 
-			System.out.print("'\n");
+			System.out.println(inmsg);
 			in.close();
 		}
 		catch(Exception e) {
 			System.out.print("Whoops! It didn't work!\n");
+			System.out.print(e.getMessage());
 		}
 	}
 }
