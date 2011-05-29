218174ecd4c1658e2f92720844b021c76c617255
Piotr Godlewski <pg290637@students.mimuw.edu.pl>
2011-05-29 21:58:16 +0200

    Zapomniałem dodać nowe klasy

---
+++ src/session/Message.java
@@ -0,0 +1,20 @@
+package session;
+
+public class Message {
+
+	private String author;
+	private String message;
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getAuthor(){
+		return author;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getMessage(){
+		return message;
+	}
+}
---
+++ src/session/Player.java
@@ -0,0 +1,12 @@
+package session;
+
+public class Player {
+	private String name;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+}
